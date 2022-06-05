23fbdc69665d238b17e0f5e35a3b07ebfc2ea529
lioarce01 <lioarce1@gmail.com>
2022-06-05 15:27:20 -0300

    enchi

---
+++ src/components/CheckoutForm/AddressForm.jsx
@@ -0,0 +1,29 @@
+import React from "react";
+import {
+  InputLabel,
+  Select,
+  MenuItem,
+  Button,
+  Grid,
+  Typography,
+} from "@material-ui/core";
+import { useForm, FormProvider } from "react-hook-form";
+import FormInput from "./CustomTextField";
+
+const AddressForm = () => {
+  const methods = useForm();
+
+  return (
+    <>
+      <FormProvider {...methods}>
+        <form onSubmit=" ">
+          <Grid container spacing={3}>
+            <FormInput required name="firstName" label="First name" />
+          </Grid>
+        </form>
+      </FormProvider>
+    </>
+  );
+};
+
+export default AddressForm;
